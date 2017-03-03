3b4869b067c8aef22edb8c2f0334b518f96dd85b
Jason Butwell <jasonbutwell@gmail.com>
2017-03-03 00:32:57 +0000

    Added URI specific constants to MovieContract class.

--- app/src/main/java/com/jasonbutwell/popularmovies/Database/MovieContract.java
+++ app/src/main/java/com/jasonbutwell/popularmovies/Database/MovieContract.java
@@ -1,5 +1,6 @@
 package com.jasonbutwell.popularmovies.Database;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -7,7 +8,17 @@ import android.provider.BaseColumns;
  */
 
 public class MovieContract {
+
+    public static final String SCHEME = "content://";
+    public static final String AUTHORITY = "com.jasonbutwell.popularmovies";
+    public static final Uri BASE_CONTENT_URI = Uri.parse(SCHEME + AUTHORITY);
+
+    public static final String PATH_MOVIES = "movies";
+
     public static final class MovieEntry implements BaseColumns {
+
+        // Scheme + Authority + Path
+        public static final Uri CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH_MOVIES).build();
 
         public static final String TABLE_NAME = "movies";
 
